e038b240b3db4f1b108205a1c7bb6457b6b53a81
Calmarius <calmarius@calmarius.net>
2017-10-26 08:56:07 +0200

    Add unit test.

--- roundpower2.c
+++ roundpower2.c
@@ -13,3 +13,19 @@ uint64_t roundUpP2(uint64_t x)
 
     return x;
 }
+
+#include <assert.h>
+
+int main()
+{
+    assert(roundUpP2(0) == 0);
+    assert(roundUpP2(1) == 1);
+    assert(roundUpP2(2) == 2);
+    assert(roundUpP2(3) == 4);
+    assert(roundUpP2(69) == 128);
+    assert(roundUpP2(50) == 64);
+    assert(roundUpP2(0x7FFFFFFF) == 0x80000000);
+    assert(roundUpP2(0x40000000) == 0x40000000);
+
+    return 0;
+}
