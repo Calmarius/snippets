a932bc26fc0ca66deb5e685f8d96cc9f38cc9eae
Calmarius <calmarius@calmarius.net>
2017-11-06 16:28:36 +0100

    Fixed warnings in FNV hash.

--- fnvhash.c
+++ fnvhash.c
@@ -51,8 +51,8 @@ int main()
     const char *testStr = "Hello world!";
     size_t n = strlen(testStr);
     
-    assert(fnv32(testStr, n) == 0x7a78f512);
-    assert(fnv64(testStr, n) == 0x9e527e14572072d2);
+    assert(fnv32((const uint8_t*)testStr, n) == 0x7a78f512);
+    assert(fnv64((const uint8_t*)testStr, n) == 0x9e527e14572072d2);
 }
 
 #endif
