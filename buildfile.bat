7958432258cdf9ee6619772144c525a1812c415c
Calmarius <calmarius@calmarius.net>
2018-03-03 13:35:10 +0100

    Preliminary bat file for Windows builds.

---
+++ buildfile.bat
@@ -0,0 +1,18 @@
+@ECHO off
+
+IF NOT DEFINED INCLUDE (
+    ECHO You must run vcvarsall.bat in your Visual Studio build tools, before you can use this script.
+    GOTO eof
+)
+
+IF [%1] == [] (
+    ECHO You need to provide the file to compile.
+    GOTO eof
+)
+
+SET FILENAME=%1
+
+
+cl -I %CD% -W4 -WX -DUNIT_TEST -Fetestprogram.exe %FILENAME%
+
+:eof
